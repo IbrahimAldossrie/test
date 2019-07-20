c4051be10c1be5864e16a77907a4a7d5b2627928
ibrahim <ibrahimaldossrie@gmail.com>
2019-07-20 12:00:48 +0300

    added file
    added file to test kraken

---
+++ firsttest.kt
@@ -0,0 +1,23 @@
+fun main(){
+    print("Enter number of User: ")
+    val numberOfUser= readLine()!!.toInt()
+    for(userID in 1..numberOfUser){
+        print("$userID- enter your name: ")
+        val name = readLine()!!.toString()
+        print("Where are you from: ")
+        val from= readLine()!!.toString()
+        var petsName:String?=""
+        print("Enter number of pets: ")
+        val numberOfPets = readLine()!!.toInt()
+        for(petnumber in 1..numberOfPets){
+            print("enter pet $petnumber: ")
+            petsName= petsName + readLine()!!.toString() + ", "
+        }
+        println("===    User Info   ===")
+        println("name: $name")
+        println("From: $from")
+        println("pets name: $petsName")
+        if (petsName!!.contains("cat"))
+            println("we have program for your cat")
+    }
+}
